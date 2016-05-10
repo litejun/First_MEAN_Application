1eff792b2f41688f2d459d545d99af2afc7daa64
litejun <litejun24@gmail.com>
2016-05-10 09:44:01 +0900

    connect db

---
+++ app/models/user.server.model.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+	firstName: String,
+	lastName: String,
+	email: {
+		type: String,
+		index: true,
+		match: /.+\@.+\..+/
+	},
+	role: {
+		type: String,
+		enum: ['Admin', 'Owner', 'User']
+	},
+	username: {
+		type: String,
+		trim: true,
+		unique: true,
+		required: true
+	},
+	password: String,
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	website: {
+		type: String,
+		get: function(url){
+			if(!url){
+				return url;
+			} else{
+				if(url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) { url = 'http://' + url;
+			}
+
+			return url;
+			}
+		}
+	}
+});
+
+UserSchema.set('toJSON', { getters: true });
+mongoose.model('User', UserSchema);
